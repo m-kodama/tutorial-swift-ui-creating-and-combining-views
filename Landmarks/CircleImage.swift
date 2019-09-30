b41206ad98d576b8837c85fe91b298c4e44fd5ea
m-kodama <m-kodama@rhizo-me.com>
2019-09-30 14:44:32 +0900

    complete section 4

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,24 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by m-kodama on 2019/09/30.
+//  Copyright © 2019 m-kodama. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -16,6 +16,7 @@ struct ContentView: View {
             HStack {
                 Text("Joshua Tree National Park")
                     .font(.subheadline)
+                Spacer()
                 Text("California")
                     .font(.subheadline)
             }
