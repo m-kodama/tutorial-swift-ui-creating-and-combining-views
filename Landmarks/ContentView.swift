1040116554fc6d488a0399a246db943fbf21aea5
m-kodama <m-kodama@rhizo-me.com>
2019-09-30 13:55:59 +0900

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by m-kodama on 2019/09/30.
+//  Copyright © 2019 m-kodama. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
