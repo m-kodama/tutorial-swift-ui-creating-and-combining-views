fe50d21a22ee2bf1d5c10dabdc80d2905585f5ea
m-kodama <m-kodama@rhizo-me.com>
2019-09-30 14:58:12 +0900

    complete section 5

---
+++ Landmarks/MapView.swift
@@ -0,0 +1,30 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by m-kodama on 2019/09/30.
+//  Copyright © 2019 m-kodama. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> MKMapView {
+        MKMapView(frame: .zero)
+    }
+    
+    func updateUIView(_ view: MKMapView, context: UIViewRepresentableContext<MapView>) {
+        let coordinate = CLLocationCoordinate2D(latitude: 34.011286, longitude: -116.166868)
+        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        view.setRegion(region, animated: true)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
