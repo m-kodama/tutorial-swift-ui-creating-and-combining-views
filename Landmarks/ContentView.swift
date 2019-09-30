02ab55bb8817dd3d004d85a528454697fcd07595
m-kodama <m-kodama@rhizo-me.com>
2019-09-30 15:11:42 +0900

    complete all section !!

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -10,18 +10,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Turtle Rock")
-                .font(.title)
-            HStack {
-                Text("Joshua Tree National Park")
-                    .font(.subheadline)
-                Spacer()
-                Text("California")
-                    .font(.subheadline)
+        VStack {
+            // マップ
+            MapView()
+                .edgesIgnoringSafeArea(.top)
+                .frame(height: 300)
+            
+            // CircleImage
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            // タイトルおよびサブタイトル
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
             }
-        }
         .padding()
+        
+        Spacer()
+        }
     }
 }
 
